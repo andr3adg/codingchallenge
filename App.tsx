2c2ed22fbce7caa98e386a4574e2714a32f89b78
André Gonçalves <andreadg96@outlook.com>
2024-06-08 13:02:16 +0100

    persist logic

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import RootNavigation from './src/utils/RootNavigation';
-import {store} from './src/modules/store/store';
+import {store, persistor} from './src/modules/store/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
 import {Provider} from 'react-redux';
 import Toast from 'react-native-toast-message';
+import Loader from './src/components/ui/Loader';
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <SafeAreaView style={styles.appMainContainer}>
-        <RootNavigation />
-        <Toast />
-      </SafeAreaView>
+      <PersistGate persistor={persistor} loading={<Loader />}>
+        <SafeAreaView style={styles.appMainContainer}>
+          <RootNavigation />
+          <Toast />
+        </SafeAreaView>
+      </PersistGate>
     </Provider>
   );
 }
